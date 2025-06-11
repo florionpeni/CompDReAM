d0d1689747cd659556fd053c277f39873d90f023
Florion Peni (Linux) <f.peni@ucl.ac.uk>
2025-06-11 16:41:28 +0100

    Fix gdrive download flags for get_cache.sh

--- get_cache.sh
+++ get_cache.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
-echo "Downloading cache files from Google Drive..."
+echo "Downloading large cache files hosted from Google Drive..."
 
-# Create data folder if missing
-mkdir -p data
-
-# Download df_chembl.csv
+# df_chembl.csv
 if [ ! -f data/df_chembl.csv ]; then
     echo "Downloading df_chembl.csv..."
-    curl -L -o data/df_chembl.csv "https://drive.google.com/uc?export=download&id=1eu-jaosbrOfSKQ49_duSOB0lmCZlydJe"
+    gdrive files download 1eu-jaosbrOfSKQ49_duSOB0lmCZlydJe --destination data/
 else
     echo "data/df_chembl.csv already exists."
 fi
 
-# Download sql_chembl_cache.parquet
+# sql_chembl_cache.parquet
 if [ ! -f data/sql_chembl_cache.parquet ]; then
     echo "Downloading sql_chembl_cache.parquet..."
-    curl -L -o data/sql_chembl_cache.parquet "https://drive.google.com/uc?export=download&id=1YP5RBI2H51OYawkhkxPv-uJcd-bPiAzK"
+    gdrive files download 1YP5RBI2H51OYawkhkxPv-uJcd-bPiAzK --destination data/
 else
     echo "data/sql_chembl_cache.parquet already exists."
 fi
 
-echo "Cache files are ready!"
+echo "All cache files are ready!"
 
