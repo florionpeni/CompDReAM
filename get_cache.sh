f1f4d03fcddd1497d12300bd211e85c6d0664261
Florion Peni <florionpeni@eduroam-int-dhcp-97-14-78.ucl.ac.uk>
2025-06-11 15:44:13 +0100

    Update on cache files and script to get cache files

--- get_cache.sh
+++ get_cache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "📥 Downloading cache files from Google Drive..."
+echo "Downloading cache files from Google Drive..."
 
 # Create data folder if missing
 mkdir -p data
@@ -10,7 +10,7 @@ if [ ! -f data/df_chembl.csv ]; then
     echo "Downloading df_chembl.csv..."
     curl -L -o data/df_chembl.csv "https://drive.google.com/uc?export=download&id=1eu-jaosbrOfSKQ49_duSOB0lmCZlydJe"
 else
-    echo "✅ data/df_chembl.csv already exists."
+    echo "data/df_chembl.csv already exists."
 fi
 
 # Download sql_chembl_cache.parquet
@@ -18,8 +18,8 @@ if [ ! -f data/sql_chembl_cache.parquet ]; then
     echo "Downloading sql_chembl_cache.parquet..."
     curl -L -o data/sql_chembl_cache.parquet "https://drive.google.com/uc?export=download&id=1YP5RBI2H51OYawkhkxPv-uJcd-bPiAzK"
 else
-    echo "✅ data/sql_chembl_cache.parquet already exists."
+    echo "data/sql_chembl_cache.parquet already exists."
 fi
 
-echo "🎉 Cache files are ready!"
+echo "Cache files are ready!"
 
